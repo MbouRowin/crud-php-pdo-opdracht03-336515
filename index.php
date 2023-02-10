e49c4747e2e995aba433fbc4d66110b469c59ceb
MbouRowin <117640285+MbouRowin@users.noreply.github.com>
2023-02-10 11:30:58 +0100

    Remove old files

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 
 require_once "database.php";
 
-$stmt = $pdo->query("SELECT * from pizza");
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $bodemformaat = $_POST["bodemformaat"] ?? "";
+    $saus = $_POST["saus"] ?? "";
+    $pizzatopping = $_POST["pizzatopping"] ?? "";
+    $kruiden = implode(", ", $_POST["kruiden"] ?? []);
+
+    $stmt = $pdo->prepare("INSERT INTO pizza (bodemformaat, saus, pizzatopping, kruiden) VALUES (?, ?, ?, ?)");
+    $stmt->bindValue(1, $bodemformaat);
+    $stmt->bindValue(2, $saus);
+    $stmt->bindValue(3, $pizzatopping);
+    $stmt->bindValue(4, $kruiden);
+
+    $stmt->execute();
+
+    header("Refresh: 2; url=/");
+    die("De pizza is aangemaakt.");
+}
 
 ?>
 <!doctype html>
@@ -17,34 +33,58 @@ $stmt = $pdo->query("SELECT * from pizza");
 
 <body>
     <div class="container">
-        <h1 class="text-center">Maak je eigen pizza</h1>
-        <a href="create.php">Create</a>
+        <h1 class="text-center">Invoer Achtbaan</h1>
 
-        <table class="table">
-            <tr>
-                <th>ID</th>
-                <th>Bodemformaat</th>
-                <th>Saus</th>
-                <th>Pizzatopping</th>
-                <th>Kruiden</th>
-                <th>Update</th>
-                <th>Delete</th>
-            </tr>
-            <?php while ($row = $stmt->fetch()) : ?>
-                <tr>
-                    <td><?= htmlspecialchars($row["id"]) ?></td>
-                    <td><?= htmlspecialchars($row["bodemformaat"]) ?></td>
-                    <td><?= htmlspecialchars($row["saus"]) ?></td>
-                    <td><?= htmlspecialchars($row["pizzatopping"]) ?></td>
-                    <td><?= htmlspecialchars($row["kruiden"]) ?></td>
-                    <td><a href="update.php?id=<?= $row["id"] ?>">Update</a></td>
-                    <td><a href="delete.php?id=<?= $row["id"] ?>">Delete</a></td>
-                </tr>
-            <?php endwhile ?>
-        </table>
+        <form method="post">
+            <div class="mb-3">
+                <label for="achtbaan" class="form-label">Naam Achtbaan</label>
+                <input type="text" name="achtbaan" id="achtbaan" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="pretpark" class="form-label">Naam Pretpark</label>
+                <input type="text" name="pretpark" id="pretpark" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="land" class="form-label">Naam Land</label>
+                <input type="text" name="land" id="land" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="topsnelheid" class="form-label">Topsnelheid (km/u)</label>
+                <input type="number" min="1" max="200" name="topsnelheid" id="topsnelheid" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="hoogte" class="form-label">Hoogte</label>
+                <input type="number" min="1" max="200" name="hoogte" id="hoogte" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="opening" class="form-label">Datum eerste opening</label>
+                <input type="date" name="opening" id="opening" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="cijfer" class="form-label">Cijfer voor achtbaan</label>
+                <input type="range" min="1" max="10" step="0.1" name="cijfer" id="cijfer" class="form-range">
+                <p id="cijfer-value"></p>
+            </div>
+
+            <div class="d-grid">
+                <button class="btn btn-primary">Sla op</button>
+            </div>
+        </form>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <script>
+        const cijfer = document.getElementById("cijfer");
+        const cijferValue = document.getElementById("cijfer-value");
+        cijferValue.innerHTML = cijfer.value;
+        cijfer.addEventListener("input", () => cijferValue.innerHTML = cijfer.value);
+    </script>
 </body>
 
 </html>
